aaa30d5674a7349fd1585c124ed7f9e17b2f9c99
Your Name <brightsy04@gmail.com>
2026-04-21 02:41:12 -0700

    Milestone 2: loop until deque is empty

--- main.cpp
+++ main.cpp
@@ -12,9 +12,32 @@ int main() {
 
     deque<car> lane;
 
+    // Milestone 1: create deque and populate with two Car objects
     for (int i = 0; i < INITIAL_QUEUE_SIZE; i++) {
         lane.push_back(car());
     }
+
+    cout << "Initial queue:\n";
+    printQueue(lane);
+    cout << endl;
+
+    
+    int timePeriod = 0;
+    // Milestone 2: loop until deque is empty
+    while (!lane.empty()) {
+        timePeriod++;
+
+        int chance = rand() % 100;  // 0~99
+
+        cout << "Time: " << timePeriod << " Operation: ";
+
+        if (chance < 55) {
+            // 55% probability: front car pays and leaves
+           
+        } else {
+            // 45% probability: another car joins
+           
+        }
 
     return 0;
 }
