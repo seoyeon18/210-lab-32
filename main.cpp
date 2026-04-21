052008c66301af15ff47493499d0493def0e1991
Your Name <brightsy04@gmail.com>
2026-04-21 02:19:56 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "car.h"
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    deque<car> lane;
+
+    return 0;
+}
