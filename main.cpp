d4fef92e8ba9cf73c5c7219ecc10fe9bc60ca0be
Your Name <brightsy04@gmail.com>
2026-04-21 02:24:00 -0700

    second commit

--- main.cpp
+++ main.cpp
@@ -5,11 +5,16 @@
 #include "car.h"
 
 using namespace std;
+const int INITIAL_QUEUE_SIZE = 2;
 
 int main() {
     srand(time(0));
 
     deque<car> lane;
+
+    for (int i = 0; i < INITIAL_QUEUE_SIZE; i++) {
+        lane.push_back(car());
+    }
 
     return 0;
 }
